e0fcf1b625e2b13cf5f4762cd98f28846eac5dd4
Kristian Lein-Mathisen <kristianlein@gmail.com>
2019-10-12 19:01:10 +0200

    chicken-android: first version of helper program
    this is just to help analysis. it's very satisfying for me to see this
    in action:
    
    ```bash
    klm@kth ~/p/c/c/egg  ➤ chicken-install -s
    klm@kth ~/p/c/c/egg  ➤ chicken-android manifest template-sotest.apk
    (@ns ("android" "http://schemas.android.com/apk/res/android")
         (manifest
           (@ (versionCode 1)
              (versionName "1.0")
              (compileSdkVersion 28)
              (compileSdkVersionCodename "9")
              (package "org.call_cc.template.sotest")
              (platformBuildVersionCode 28)
              (platformBuildVersionName 9))
           (uses-sdk (@ (minSdkVersion 21) (targetSdkVersion 28)))
           (application
             (@ (label (ref 2131099648))
                (icon (ref 2131034112))
                (debuggable #t)
                (allowBackup #t)
                (supportsRtl #t)
                (roundIcon (ref 2131034113)))
             (activity
               (@ (name "org.call_cc.template.sotest.MainActivity"))
               (intent-filter
                 (@)
                 (action (@ (name "android.intent.action.MAIN")))
                 (category (@ (name "android.intent.category.LAUNCHER"))))))))
    ```

---
+++ chicken-android.scm
@@ -0,0 +1,22 @@
+(import (only (chicken pretty-print) pp)
+        (only (chicken process-context) command-line-arguments)
+        (android minizip)
+        (android bxml)
+        (only chicken.io read-string)
+        matchable)
+
+(match (command-line-arguments)
+
+  (("manifest" file.apk)
+   (define path (car (command-line-arguments)))
+   (unzip-walk file.apk
+               (lambda (uz path port)
+                 (if (equal? path "AndroidManifest.xml")
+                     (pp (bxml->sxml (read-string #f port)))))))
+
+  (else
+   (print "no match for command-line-arguments " (command-line-arguments))
+   (print "usage:
+  manifest <file.apk>
+     prints app.apk's AndroidManifest.xml as sxml")
+   (exit 1)))
