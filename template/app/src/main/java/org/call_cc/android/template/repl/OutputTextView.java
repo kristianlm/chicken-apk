c0ac5c0b0453d4d5bd61e0e7d507b021d32797c3
Kristian Lein-Mathisen <kristianlein@gmail.com>
2019-11-13 16:34:06 +0100

    template ui: discard terminal output when things become slow
    I don't know if this is a good idea, most terminals have a hardcoded
    limit of like 500 000 lines. but it's not really what you care about:
    you want to keep as much history as possible without things becoming
    laggy. so let's try this and see how it goes.
    
    instead of deleting the top half, we could keep it somewhere in case
    it's important.

--- template/app/src/main/java/org/call_cc/android/template/repl/OutputTextView.java
+++ template/app/src/main/java/org/call_cc/android/template/repl/OutputTextView.java
@@ -1,9 +1,12 @@
 package org.call_cc.android.template.repl;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.text.SpannableStringBuilder;
 import android.util.AttributeSet;
 import android.widget.TextView;
 
+// TODO: move EscapeSequence handling here
 public class OutputTextView extends TextView {
     public OutputTextView(Context context) {
         super(context);
@@ -29,4 +32,29 @@ public class OutputTextView extends TextView {
     public boolean bringPointIntoView(int offset) {
         return false;
     }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        long nt = System.nanoTime();
+        super.onDraw(canvas);
+        nt = System.nanoTime() - nt;
+        if(nt > 1000000* 20/*ms*/ ) {
+            CharSequence t = getText();
+            if(t.length() > 1024* 10/*KiB*/) {
+                if (t instanceof SpannableStringBuilder) {
+                    SpannableStringBuilder ssb = (SpannableStringBuilder) t;
+                    // we're taking too long to draw text, probably because it's too much text there.
+                    // so we delete half of it. simple and stupid, but keeps the FPS from dropping.
+                    int start = (ssb.length()/2);
+                    int pos = start;
+                    for( ; pos < start+1024; pos++ )
+                        if(ssb.charAt(pos) == '\n')
+                            break;
+                    ssb.delete(0, pos);
+                }
+            }
+        }
+    }
+
+
 }
